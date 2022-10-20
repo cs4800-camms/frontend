ee02cc893bd439140bad25d245e15cdf4555919d
marysetian <marysetian82@gmail.com>
2022-10-20 09:35:29 -0700

    Added delete trip functionality

--- my-app/src/components/trips/TripItem.js
+++ my-app/src/components/trips/TripItem.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./TripItem.module.css";
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 export default function TripItem(props) {
+
   return (
     <div className={`text-center ${classes.align}`}>
       <img src={props.image} className={`img-fluid ${classes.image}`} alt={props.title} />
@@ -14,7 +15,7 @@ export default function TripItem(props) {
         <i id="btnGroupDrop1" type="icon" className={`btn btn-sm dropdown-toggle ${classes.button}`} data-bs-toggle="dropdown" aria-expanded="false"></i>
         <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
           <li><a class="dropdown-item" href="/edit">Edit trip info</a></li>
-          <li><a class="dropdown-item" href="#">Delete trip</a></li>
+          <li><a onClick={() => props.handleRemove(props.id)} class="dropdown-item" href="#">Delete trip</a></li>
         </ul>
       </div>
     </div>
